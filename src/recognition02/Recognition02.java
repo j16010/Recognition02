de94fb8e283b26a9c4edccdf27677ec30175c52d
j16010 <j16010@sangi.jp>
2018-05-17 12:35:50 +0900

    Shizuoka University Java 2017

---
+++ src/recognition02/Recognition02.java
@@ -0,0 +1,38 @@
+package recognition02;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
+
+public class Recognition02 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		VisualRecognition service = new VisualRecognition("2018-03-19");
+		service.setApiKey("j16010");
+
+		InputStream imagesStream = null;
+		try {
+			imagesStream = new FileInputStream("img/fruitbowl.jpg");
+		} catch (FileNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		ClassifyOptions classifyOptions = new ClassifyOptions.Builder()
+		  .imagesFile(imagesStream)
+		  .imagesFilename("fruitbowl.jpg")
+		  .threshold((float) 0.6)
+		  .owners(Arrays.asList("IBM"))
+		  .build();
+		ClassifiedImages result = service.classify(classifyOptions).execute();
+		System.out.println(result);
+
+	}
+
+}
