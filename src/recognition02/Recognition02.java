3dfdaafe178d27ce2e1ab007732faa31fbbc4b85
j16010 <j16010@sangi.jp>
2018-05-24 18:40:44 +0900

    5/24

--- src/recognition02/Recognition02.java
+++ src/recognition02/Recognition02.java
@@ -2,9 +2,12 @@ package recognition02;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
@@ -15,7 +18,7 @@ public class Recognition02 {
 		// TODO 自動生成されたメソッド・スタブ
 		
 		VisualRecognition service = new VisualRecognition("2018-03-19");
-		service.setApiKey("j16010");
+		service.setApiKey("93699c8911885458e143a7420980bebe1e5a6bc4");
 
 		InputStream imagesStream = null;
 		try {
@@ -31,8 +34,39 @@ public class Recognition02 {
 		  .owners(Arrays.asList("IBM"))
 		  .build();
 		ClassifiedImages result = service.classify(classifyOptions).execute();
+		
 		System.out.println(result);
-
+		String s = String.valueOf(result);
+		ObjectMapper mapper = new ObjectMapper();
+		
+		JsonNode node = null;
+		try {
+			 node = mapper.readTree(s);
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		
+		String class1= node.get("images").get(0).get("classifiers").get(0).get("classes").get(0).get("class").toString();
+		System.out.println("class:"+class1);
+		
+		double score1= node.get("images").get(0).get("classifiers").get(0).get("classes").get(0).get("score").asDouble();
+		System.out.println("class:"+score1);
+		
+		
+		String class2= node.get("images").get(0).get("classifiers").get(0).get("classes").get(1).get("class").toString();
+		System.out.println("class:"+class2);
+		
+		double score2= node.get("images").get(0).get("classifiers").get(0).get("classes").get(1).get("score").asDouble();
+		System.out.println("class:"+score2);
+		
+		
+		String class3= node.get("images").get(0).get("classifiers").get(0).get("classes").get(2).get("class").toString();
+		System.out.println("class:"+class3);
+		
+		double score3= node.get("images").get(0).get("classifiers").get(0).get("classes").get(2).get("score").asDouble();
+		System.out.println("class:"+score3);
+		
 	}
 
 }
